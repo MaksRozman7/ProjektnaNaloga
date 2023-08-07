42a6872c11bc17ac4259605eb526f383a8251b4d
Maks Rozman <maksrozman.mr@gmail.com>
2023-08-07 17:09:25 +0200

    iskanje imen podjetij

--- podjetja.py
+++ podjetja.py
@@ -1,4 +1,15 @@
 from bs4 import BeautifulSoup
+import requests
+
+with open("podatkiZaPodjetja.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+result = doc.find_all(class_="card-title")
+print(result)
+
+
+
+
 
 
 
