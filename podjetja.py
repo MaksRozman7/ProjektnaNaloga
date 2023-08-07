6f3bf0949971c07eaae9aa25ecee7e4dd9afe0b7
Maks Rozman <maksrozman.mr@gmail.com>
2023-08-07 17:23:22 +0200

    dobil seznam imen

--- podjetja.py
+++ podjetja.py
@@ -5,7 +5,13 @@ with open("podatkiZaPodjetja.html", "r") as f:
     doc = BeautifulSoup(f, "html.parser")
 
 result = doc.find_all(class_="card-title")
-print(result)
+
+
+sez=[]
+for stvar in doc.find_all(class_="card-title"):
+    sez.append(stvar.string.replace(" ","").replace("\n", ""))
+
+print(sez[1:-3])
 
 
 
