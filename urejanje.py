58679ee1398d174ced2d6f0ae8ed9aa3435ed008
Maks Rozman <maksrozman.mr@gmail.com>
2023-08-25 15:59:28 +0200

    začetek urejanja

--- urejanje.py
+++ urejanje.py
@@ -4,4 +4,16 @@ podjetja = pd.read_csv("podatkiPodjetja.csv")
 
 pd.options.display.max_rows = 20
 
-print(podjetja.vrednost >= 100)
+
+
+firme_po_državah = podjetja.groupby("država").agg({"ime": "count", "vrednost": "sum"})
+
+skupna_vrednost_držav = firme_po_državah.sort_values("vrednost", ascending=False)
+
+podjetja_na_državo = firme_po_državah.sort_values("ime", ascending=False)
+    
+sektor_vrednosti = podjetja.groupby("sektor").agg({"ime": "count", "vrednost": "sum"}).sort_values("vrednost", ascending=False)
+
+sektor_povprečna_vrednost = podjetja.groupby("sektor")["vrednost"].mean().sort_values(ascending=False)
+
+print(sektor_povprečna_vrednost)
